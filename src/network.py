557a56f5fa032fbec594ea0e0c27fe8b69ed8c16
Connor Henderson <connor.henderson@talkiatry.com>
2022-06-12 12:03:52 -0400

    added feedforward method

--- src/network.py
+++ src/network.py
@@ -1,5 +1,5 @@
-import random
 import numpy as np
+from helpers import sigmoid
 
 class Network: 
     def __init__(self, layer_lengths):
@@ -7,3 +7,25 @@ class Network:
         self.layer_lengths = layer_lengths
         self.biases = [np.random.rand(x, 1) for x in layer_lengths[1:]]
         self.weights = [np.random.rand(x, y) for x, y in zip(layer_lengths[:-1], layer_lengths[1:])]
+    
+    def feedforward(self, a):
+        # this feeds an activation (a) through the network,
+        # with the intial activation being the input to the network
+        for w, b in zip(self.weights, self.biases):
+            a = sigmoid(np.dot(w, a) + b)
+        return a
+
+
+
+
+
+
+
+
+
+# TEST AREA
+
+import random
+rand_output = np.random.rand(10, 1)
+rand_sigmoid = sigmoid(rand_output)
+print(rand_output, '\n', rand_sigmoid)
