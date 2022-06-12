5ca624caae020b454517589270f9b7b99458c8cc
Connor Henderson <connor.henderson@talkiatry.com>
2022-06-12 11:08:16 -0400

    swapped w and b

--- src/network.py
+++ src/network.py
@@ -5,5 +5,5 @@ class Network:
     def __init__(self, layer_lengths):
         self.num_layers = len(layer_lengths)
         self.layer_lengths = layer_lengths
-        self.weights = [np.random.rand(x, 1) for x in layer_lengths[1:]]
-        self.biases = [np.random.rand(x, y) for x, y in zip(layer_lengths[:-1], layer_lengths[1:])]
+        self.biases = [np.random.rand(x, 1) for x in layer_lengths[1:]]
+        self.weights = [np.random.rand(x, y) for x, y in zip(layer_lengths[:-1], layer_lengths[1:])]
