df7eeb1c94af0a38b38f7a2dc9ec8d77c99ab4b9
Connor Henderson <connor.henderson@talkiatry.com>
2022-06-12 10:37:31 -0400

    fixed network init for w and b

--- src/network.py
+++ src/network.py
@@ -1,8 +1,9 @@
 import random
+import numpy as np
 
 class Network: 
     def __init__(self, layer_lengths):
         self.num_layers = len(layer_lengths)
         self.layer_lengths = layer_lengths
-        self.weights = [random.randrange(-1, 1) for x in range(len(layer_lengths) - 1)]
-        self.biases = [random.randrange(-1, 1) for x in range(len(layer_lengths) - 1)]
+        self.weights = [np.random.rand(x, 1) for x in layer_lengths[1:]]
+        self.biases = [np.random.rand(x, y) for x, y in zip(layer_lengths[:-1], layer_lengths[1:])]
