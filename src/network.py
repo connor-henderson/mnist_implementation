018b70fad79de02759452c9ffbabd4d353933b49
Connor Henderson <connor.henderson@talkiatry.com>
2022-06-14 10:45:34 -0400

    wrote update mini batch method

--- src/network.py
+++ src/network.py
@@ -29,12 +29,15 @@ class Network:
     def backprop(self):
         pass
 
-    def update_mini_batch(self):
-        pass
-
-
-
-
+    def update_mini_batch(self, mini_batch, lr):
+        nabla_b = [np.zeros(b.shape) for b in self.biases]
+        nabla_w = [np.zeros(w.shape) for w in self.weights]
+        for guess, actual in mini_batch:
+            delta_nabla_b, delta_nabla_w = self.backprop(guess, actual)
+            nabla_b = [nb+dnb for nb, dnb in zip(nabla_b, delta_nabla_b)]
+            nabla_w = [nw+dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
+        self.weights = [w-(lr/len(mini_batch))*nw for w, nw in zip(self.weights, nabla_w)]
+        self.biases = [b-(lr/len(mini_batch))*nb for b, nb in zip(self.biases, nabla_b)]
 
 
 
