6dc0918798375d6860d2711b56180346e120b05d
Connor Henderson <connor.henderson@talkiatry.com>
2022-06-14 10:59:50 -0400

    updated update_mini_batch, SGD and added evaluate to network

--- src/network.py
+++ src/network.py
@@ -10,24 +10,24 @@ class Network:
         self.weights = [np.random.rand(x, y) for x, y in zip(layer_lengths[:-1], layer_lengths[1:])]
     
     def feedforward(self, a):
-        # this feeds an activation (a) through the network,
-        # with the intial activation being the input to the network
+        # is only used in the evaluate method -- update_mini_batch calls backprop which calls its own version
+        # of feedforward which is the same in terms of functionality, but also stores values that are necessary 
+        # for other backprop steps
         for w, b in zip(self.weights, self.biases):
             a = sigmoid(np.dot(w, a) + b)
         return a
 
-    # implement SGD
-    def SGD(self, lr=1, mini_batch_size=10, epochs=10, training_data=None):
+    def SGD(self, lr=1, mini_batch_size=10, epochs=10, training_data=None, test_data=None):
         training_data_length = len(training_data)
         for i in range(1, epochs + 1):
             shuffled_training_data = random.shuffle(training_data)
-            mini_batches = [shuffled_training_data[:x] for x in range(0, training_data_length, mini_batch_size)]
+            mini_batches = [shuffled_training_data[x:x+mini_batch_size] for x in range(0, training_data_length, mini_batch_size)]
             for mini_batch in mini_batches:
                 self.update_mini_batch(mini_batch, lr)
-            print(f"Epoch {i} of {epochs}")
-
-    def backprop(self):
-        pass
+            if test_data:
+                number_correct = self.evaluate(test_data)
+                print(f"Epoch {i}: {(number_correct/mini_batch_size)*100}% correct")
+            else: print(f"Epoch {i} of {epochs}")
 
     def update_mini_batch(self, mini_batch, lr):
         nabla_b = [np.zeros(b.shape) for b in self.biases]
@@ -38,6 +38,16 @@ class Network:
             nabla_w = [nw+dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
         self.weights = [w-(lr/len(mini_batch))*nw for w, nw in zip(self.weights, nabla_w)]
         self.biases = [b-(lr/len(mini_batch))*nb for b, nb in zip(self.biases, nabla_b)]
+    
+    def backprop(self):
+        pass
+
+    def evaluate(self, test_data):
+        number_correct = 0
+        for x, y in test_data:
+            a = self.feedforward(x)
+            if a == y: number_correct += 1
+        return number_correct
 
 
 
